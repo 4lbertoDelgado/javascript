ddb2b6548d7daf0065971a7301adfecee8c2d709
4lbertoDelgado <albertodq7@gmail.com>
2018-09-19 17:49:43 -0500

    Update javascript_oriented_object.js

--- 2.JAVASCRIPT_ORIENTED_OBJECTS/javascript_oriented_object.js
+++ 2.JAVASCRIPT_ORIENTED_OBJECTS/javascript_oriented_object.js
@@ -1,5 +1,44 @@
 
+/*	
+Alcance Scope	
+-----------------	
+Cuado se le asigna un valor a una variable que nunca a sido declarada con la palabra var, esta toma una alcance global. Es una mala practica.	
+Los ambitos son solo delimitados por las llaves que encierran las funciones	
+No como en otros lenguajes que importan las llaves de los bucles o las condicionales	
+Contextos Lexicos	
+Es el contexto de alcance de las variables a medida que se va escribiendo el codigo	
+Contextos de Ejecucion	
+Es el contexto de alcance de las variables a medida que se va ejecutando el codigo	
+*/
 
+/*
+Closures	
+------------------
+Es una funcion que retorna desde otra funcion. y esta se lleva una referencia a el ambito 
+(variables, parametros y funciones) de la funcion que la retorna
+
+Esa referencia al ambito superior debe de existir, congelaser,  hasta que la funcion interna se ejecute ya que sino daria error porque no podria acceder a esas variables	
+Tienen la capacidad de acceder a todas las funciones variables y parametros que se encuentran fuera de su ambito de contexto, aun cuando la funcion padre aya terminado de ejecutarse.	
+La funcion definida en el closure recuerda la funcion donde se ha creado
+
+Formas de manejar la funcion interna:
+ a. Se puede retornar una funcion anonima 
+ b. Se puede ejecutar en el contaxto de la funcion padre
+*/
+
+function nombre(primerNombre) {	
+  var saludo = "Como estas?";	
+   return function (apellido) {	
+      console.log("Hola " + primerNombre + " " + apellido + " " + saludo);	
+  }	
+}	
+// Se puede invocar a la funcion asi	
+nombre("Pierre")("Delgado"); // El segundo parentesis hace referencia a la funcion interna	
+// O se puede invocar asi tambien	
+var variable = new nombre("Bryan");	
+console.log(variable); // aqui puedo ver que variable tiene asignada una funcion, que es la funcion anonima que la que ha retornado la funcion padre nombre	
+variable("Carrizales"); // Como se que es la funcion anonima, la invoco pasandole su parametro	
+// Al impimirse, esta utiliza sus variable parametros internos asi como las referecnias a las variables y parametros de la funcion padre	
 
 /*
 This
@@ -91,11 +130,41 @@ printName.apply( person2 ); // Scarlett Johansson
 Al usar las funciones flechas, this se asocia automaticamente al contexto inmediato superior 
 Javascript permite utilizar una variable, incluso asignarle valores, sin que esta aya sido definida previamente.
 Este comportamiento hace que dicha variable de modo IMPLICITO se declare en el entorno global
+
 Con la instruccion "use strict" evito ese comportamiento implicito
+http://www.etnassoft.com/2011/03/01/strict-mode-en-javascript/
+Por ejemplo, el modo estricto impide crear accidentalmente variables globales cuando no las iniciamos utilizando var:
+
+"use strict";
+mistypedVar = 'Hello World'; // assignment to undeclared variable mistypedVar
+
+También ahora los intentos de sobreescribir variables, métodos u objetos nativos del lenguaje lanzarán su correspondiente error:
+
+"use strict";
+NaN = 42; // NaN is read-only
+ 
+var obj = { get x() { return 17; } };
+obj.x = 5; // Setting a property that has only a getter
+ 
+delete Object.prototype; // property Object.prototype is non-configurable and can't be deleted
+
+Otro nuevo error se produce si tratamos de asignar el mismo nombre para dos propiedades de un mismo objeto:
+
+"use strict";
+var foo = { bar : 1, bar : 2 } // property name bar appears more than once in object literal
+
+En el modo estricto, el comportamiento ha cambiado y ahora los valores undefined o null remiten a la función original:
+
+"use strict";
+function foo(){
+  return this;
+}
+ 
+console.log( foo ); // foo()
+
 */
 
 // las funciones normales capturan el valor de this como el objeto global (o undefined en modo estricto).
-"use strict";
 function Persona() {            
   this.edad = 0; // 
 
@@ -107,7 +176,6 @@ Persona();
 /*
 // Mientras que las funciones flecha capturan ese valor del scope externo inmediato:
 */
-"use strict";
 function Persona(){
     this.edad = 0;
     setInterval(() => {
@@ -115,3 +183,6 @@ function Persona(){
     }, 1000);
 }
 Persona();
+
+
+
